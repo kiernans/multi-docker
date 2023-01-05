03e8d6087805bce8ad62230ae8f430854198c6db
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2023-01-04 21:35:29 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+	return (
+		<BrowserRouter>
+			<div className='App'>
+				<header className='App-header'>
+					<img src={logo} className='App-logo' alt='logo' />
+					<Link to='/'>Home</Link>
+					<Link to='/otherpage'>Other Page</Link>
+				</header>
+				<Routes>
+					<Route path='/' element={<Fib />}></Route>
+					<Route path='/otherpage' element={<OtherPage />}></Route>
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
+}
+
+export default App;
